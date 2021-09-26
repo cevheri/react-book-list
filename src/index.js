e7d1cc324c31f2f7c1bd48387521d031aacaaa82
Cevheri <cevheribozoglan@gmail.com>
2021-09-26 20:33:08 +0300

    2

--- src/index.js
+++ src/index.js
@@ -4,8 +4,29 @@ import ReactDom from "react-dom";
 // stateless functional component
 // always return JSX
 
-function Greeting() {
-  return <h4>Hello</h4>
-} 
+// function Greeting() {
+//   return <h4>Hello</h4>
+// } 
   
+
+// const Greeting=()=>{
+//   return React.createElement("h1",{},"helloo;")
+// }
+
+function Greeting(){
+  return (
+    <div>
+      <h1>hello 3</h1>
+    </div>
+  )
+}
+
+// const Greeting = () => {
+//   return React.createElement(
+//     'div',
+//     {},
+//     React.createElement('h1', {}, "hello 4")
+//   );
+// };
+
 ReactDom.render(<Greeting />, document.getElementById("root"));
