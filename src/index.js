0f68d5814aa0d3b06f97d15d6623f656ec7b3e6a
Cevheri <cevheribozoglan@gmail.com>
2021-09-26 21:40:30 +0300

    first book list hard coded

--- src/index.js
+++ src/index.js
@@ -64,5 +64,33 @@ import ReactDom from "react-dom";
 
 // ReactDom.render(<Greeting />, document.getElementById("root"));
 
+function BookList() {
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+const Image = () => {
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/91Sv0lGpX3L._AC_UL200_SR200,200_.jpg"
+      alt=""
+    />
+  );
+};
+const Title = () => <h1>The Wonderful Things You Will Be</h1>;
+const Author = () => <h4>Emily Winfield Martin</h4>;
 
-function 
+ReactDom.render(<BookList />, document.getElementById("root"));
